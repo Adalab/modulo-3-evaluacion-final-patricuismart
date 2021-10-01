92187d1f728f51d0e1366298b91761c84a7f3b0e
patricuismart <patricuismart@gmail.com>
2021-10-01 16:48:10 +0200

    Add main HTML & CSS structure

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,33 @@ import '../styles/App.scss';
 function App() {
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <header>
+        <h1 className="title">Rick and Morty</h1>
+      </header>
+      <main className="main">
+        <section className="form">
+          <form>
+            <label className="form__label" htmlFor="name">
+              Filtrar por nombre:
+            </label>
+            <input
+              className="form__input-text"
+              type="text"
+              name="name"
+              id="name"
+            />
+          </form>
+        </section>
+        <section className="card__list">
+          <article className="card__item">
+            <a href="#">
+              <img className="card__img" src="#" alt="#" title="#" />
+              <h4 className="card__title">Nombre</h4>
+              <p className="card__description">Especie</p>
+            </a>
+          </article>
+        </section>
+      </main>
     </div>
   );
 }
