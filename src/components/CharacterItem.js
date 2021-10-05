28e789fc59d87e07b9d34ab946086254495a0caf
patricuismart <patricuismart@gmail.com>
2021-10-05 12:32:28 +0200

    Add icon and css to CharacterItem

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -1,6 +1,20 @@
 import { Link } from 'react-router-dom';
+const dead = <i className="fas fa-skull skull"></i>;
+const alive = <i className="fa fa-heartbeat heart " aria-hidden="true"></i>;
+const unknown = (
+  <i className="fa fa-question-circle unknown" aria-hidden="true"></i>
+);
 
 const CharacterItem = (props) => {
+  const getStatus = () => {
+    if (props.characterData.status === 'Dead') {
+      return dead;
+    } else if (props.characterData.status === 'Alive') {
+      return alive;
+    } else {
+      return unknown;
+    }
+  };
   return (
     <Link className="card__link" to={`./character/${props.characterData.id}`}>
       <img
@@ -11,6 +25,7 @@ const CharacterItem = (props) => {
       />
       <h4 className="card__title">{props.characterData.name}</h4>
       <p className="card__description">{props.characterData.species}</p>
+      <p className="card__icon">{getStatus()}</p>
     </Link>
   );
 };
